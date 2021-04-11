a3bb2ec803775ba43b704269d138dc81118eb07e
d0rianb <dorian.beauchesne@icloud.com>
2021-04-11 17:39:08 +0200

    CLI is working

--- bin/auto-version.js
+++ bin/auto-version.js
@@ -4,7 +4,8 @@ const AutoVersion = require('../lib/main.js')
 
 const args = process.argv.slice(2).map(arg => arg.replace(/--/, ''))
 
-const version = AutoVersion.getVersion(__dirname)
-// const mode = (args.length > 0 && ['major', 'minor', 'patch'].includes(args[0])) ? args[0] : 'patch'
-// const newVersion = AutoVersion.increment(version, mode)
-// AutoVersion.setVersion(__dirname, newVersion)
+const version = AutoVersion.getVersion()
+const mode = (args.length > 0 && ['major', 'minor', 'patch'].includes(args[0])) ? args[0] : 'patch'
+const newVersion = AutoVersion.increment(version, mode)
+AutoVersion.setVersion(newVersion)
+console.log(`Update version to ${newVersion}`)
--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 const AutoVersion = require('../lib/main.js')
 
-const version = AutoVersion.getVersion(__dirname)
-const newVersion = AutoVersion.minor(version)
-AutoVersion.setVersion(__dirname, newVersion)
-console.log(newVersion)
+const version = AutoVersion.getVersion()
+console.log(version)
+// const newVersion = AutoVersion.minor(version)
+// AutoVersion.setVersion(__dirname, newVersion)
+// console.log(newVersion)
