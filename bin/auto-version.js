9b4249980602961d77957b1d844bc7dd9956e15b
d0rianb <dorian.beauchesne@icloud.com>
2021-04-13 18:20:41 +0200

    Add a --get flag to the CLI

--- bin/auto-version.js
+++ bin/auto-version.js
@@ -7,11 +7,27 @@ const AutoVersion = require('../lib/main.js')
  *    - Add a --indent option change the indentation
  */
 
+/* Usage :
+ * - npx auto-version         --> increment patch
+ * - npx auto-version --get   --> return the version (ex: 0.2.3)
+ * - npx auto-version --patch --> increment patch
+ * - npx auto-version --minor --> increment minor
+ * - npx auto-version --major --> increment major
 
-const args = process.argv.slice(2).map(arg => arg.replace(/--/, ''))
+ */
+
+const args = process.argv.slice(2).map(arg => arg.replace(/--?/, ''))
 
 const version = AutoVersion.getVersion()
-const mode = (args.length > 0 && ['major', 'minor', 'patch'].includes(args[0])) ? args[0] : 'patch'
-const newVersion = AutoVersion.increment(version, mode)
-AutoVersion.setVersion(newVersion)
-console.log(`Update version to ${newVersion}`)
+
+if (args.includes('get') || args.includes('g')) {
+    console.log(version)
+} else {
+    const mode = (args.length > 0 && ['major', 'minor', 'patch'].includes(args[0])) ? args[0] : 'patch'
+    const newVersion = AutoVersion.increment(version, mode)
+    AutoVersion.setVersion(newVersion)
+    console.log(`Update version to ${newVersion}`)
+}
+
+
+
