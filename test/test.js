5b0cb27e672ad4d74e3959b716f4838686f8a2ac
d0rianb <dorian.beauchesne@icloud.com>
2021-05-12 21:09:46 +0200

    Fix pkgDir issue

--- test/test.js
+++ test/test.js
@@ -1,7 +1,6 @@
 const AutoVersion = require('../lib/main.js')
 
 const version = AutoVersion.getVersion()
-console.log(version)
 // const newVersion = AutoVersion.minor(version)
 // AutoVersion.setVersion(__dirname, newVersion)
 // console.log(newVersion)
